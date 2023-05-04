3b2e62ccf252002ba54224b585e2b17a95ea3275
jaredmwolff <jaredwolff117@gmail.com>
2023-05-03 17:32:07 -0700

    Week 3 Learning Process

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Deli Clerk";
+        job1._company = "WinCo Foods";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Missionary";
+        job2._company = "Church of Jesus Christ of Latter-day Saints";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jared Wolff";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
