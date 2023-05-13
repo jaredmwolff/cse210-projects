8962eb39a1c1dce78374e38dc0d9053e21e0b3c8
jaredmwolff <jaredwolff117@gmail.com>
2023-05-12 22:48:04 -0700

    Due to huge time constraints, this is a flop...

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+    
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(new Entry());
+        
+    }
+
+    public void Display()
+    {
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void Save(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine(entry);
+            }
+        }
+    }
+
+    public void Load(string filename)
+    {
+        
+    }
+
+}
